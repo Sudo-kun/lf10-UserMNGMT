fdc0256865217c41da1bab2951bec33d26f41610
Brisko Bernburg <b.bernburg@davengo.com>
2023-02-21 12:01:48 +0100

    added views enum

---
+++ src/main/java/de/oszimt/lf10ContractMgmt/Views.java
@@ -0,0 +1,25 @@
+package de.oszimt.lf10ContractMgmt;
+
+import de.oszimt.lf10ContractMgmt.view.CustomerPanel;
+import de.oszimt.lf10ContractMgmt.view.LoginPanel;
+
+public enum Views {
+    LOGIN("Login", LoginPanel.class),
+    CONTACT("Edit/Create Contact", CustomerPanel.class);
+
+    private final String title;
+    private final Class<?> viewClass;
+
+    Views(String title, Class<?> viewClass) {
+        this.title = title;
+        this.viewClass = viewClass;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Class<?> getViewClass() {
+        return viewClass;
+    }
+}
