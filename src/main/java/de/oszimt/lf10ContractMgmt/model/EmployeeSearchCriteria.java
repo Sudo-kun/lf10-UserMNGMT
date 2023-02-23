6d8b5dc6cb5791237ab9cb4d6fb4754f09a55354
georg <g.schwitalla@gmx.de>
2023-02-23 14:45:25 +0100

    add search to employee
    we are now able to search for employees

---
+++ src/main/java/de/oszimt/lf10ContractMgmt/model/EmployeeSearchCriteria.java
@@ -0,0 +1,9 @@
+package de.oszimt.lf10ContractMgmt.model;
+
+import de.oszimt.lf10ContractMgmt.view.SearchCriteria;
+
+public class EmployeeSearchCriteria extends SearchCriteria {
+  public EmployeeSearchCriteria(String firstname, String lastname, String address, String email, String telefon) {
+    super();
+  }
+}
